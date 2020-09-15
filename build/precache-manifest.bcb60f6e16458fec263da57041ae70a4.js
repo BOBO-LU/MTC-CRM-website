365bbbd9022af3d67b624c256e0f22faf8c4819d
BOBO-LU <luwenkai0820@gmail.com>
2020-09-15 21:54:34 +0800

    add npm build folder

---
+++ build/precache-manifest.bcb60f6e16458fec263da57041ae70a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce126d215a323fb469a4ae31741302b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb234900c43a28481cb7",
+    "url": "/static/css/2.906be05d.chunk.css"
+  },
+  {
+    "revision": "682e905ec6ee6559245f",
+    "url": "/static/css/main.58af3114.chunk.css"
+  },
+  {
+    "revision": "eb234900c43a28481cb7",
+    "url": "/static/js/2.cbaee3ab.chunk.js"
+  },
+  {
+    "revision": "330bedb07e5a1a0bc1b9626e3271e3ee",
+    "url": "/static/js/2.cbaee3ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682e905ec6ee6559245f",
+    "url": "/static/js/main.bcacff09.chunk.js"
+  },
+  {
+    "revision": "92371cd772b3e46f904b",
+    "url": "/static/js/runtime-main.4eafc052.js"
+  },
+  {
+    "revision": "03ea0c8b70be3e920e91b1e635d6bcf0",
+    "url": "/static/media/cross.03ea0c8b.svg"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/static/media/dxicons.2b145cef.woff"
+  }
+]);
