e489e36973ed0078e8ba31bbd90d7908634d6330
BOBO-LU <luwenkai0820@gmail.com>
2020-09-18 13:16:09 +0800

    nothing

---
+++ build/precache-manifest.4443c8a65bc76127f0972b56ebfc5078.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236d5f9afc9f058955639aa4ba34e24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "785ac85da3812c7b3a2d",
+    "url": "/static/css/2.906be05d.chunk.css"
+  },
+  {
+    "revision": "ae2adc6e1cb57d2f6061",
+    "url": "/static/css/main.58af3114.chunk.css"
+  },
+  {
+    "revision": "785ac85da3812c7b3a2d",
+    "url": "/static/js/2.f156daed.chunk.js"
+  },
+  {
+    "revision": "330bedb07e5a1a0bc1b9626e3271e3ee",
+    "url": "/static/js/2.f156daed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2adc6e1cb57d2f6061",
+    "url": "/static/js/main.667bde68.chunk.js"
+  },
+  {
+    "revision": "92371cd772b3e46f904b",
+    "url": "/static/js/runtime-main.4eafc052.js"
+  },
+  {
+    "revision": "03ea0c8b70be3e920e91b1e635d6bcf0",
+    "url": "/static/media/cross.03ea0c8b.svg"
+  },
+  {
+    "revision": "1286a19fc578e6b5a92e6dcb744dec16",
+    "url": "/static/media/dxicons.1286a19f.woff2"
+  },
+  {
+    "revision": "147eb6fe61ff3af12cd8763022ae7f24",
+    "url": "/static/media/dxicons.147eb6fe.ttf"
+  },
+  {
+    "revision": "2b145cefd95f699e2eecc965fb38c8a8",
+    "url": "/static/media/dxicons.2b145cef.woff"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b35b319ba9c536110716c1d020e84be5.js"
+  "/precache-manifest.4443c8a65bc76127f0972b56ebfc5078.js"
 );
 
 self.addEventListener('message', (event) => {
