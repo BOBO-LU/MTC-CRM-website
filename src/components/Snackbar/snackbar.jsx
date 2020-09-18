aaef566bfec87cc8e77c647622f4f36928ec8df8
BOBO-LU <luwenkai0820@gmail.com>
2020-09-18 13:34:33 +0800

    delete /

--- src/components/Snackbar/snackbar.jsx
+++ src/components/Snackbar/snackbar.jsx
@@ -18,7 +18,7 @@ function Snackbar(props) {
     const { enqueueSnackbar } = useSnackbar();
     const tobackendget = () => {
         console.log("start tobackendget");
-        fetch("https://ebappbackend.azurewebsites.net/")
+        fetch("https://ebappbackend.azurewebsites.net")
             .then((res) => res.json())
             .then((data) => {
                 console.log("fetch data: ", data);
@@ -36,7 +36,7 @@ function Snackbar(props) {
         console.log("start tobackendpost");
 
         axios
-            .post("https://ebappbackend.azurewebsites.net/", {
+            .post("https://ebappbackend.azurewebsites.net", {
                 method: "post",
                 data: {
                     startTime: startTime,
